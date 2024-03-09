82458bbd363d2fca074c7e9f00f66300705750b9
THESV7 <vadhelsmit@gmail.com>
2024-03-09 17:06:42 +0530

    video 54 done

---
+++ Video 54 - Introduction to JavaScript & Installing Node.js/script.js
@@ -0,0 +1,18 @@
+alert("Hey I am a Box")
+
+console.log("Code is running....");
+console.log("Code is also running....");
+console.log("Code is looking like wow....");
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+console.log("Your number is " + a);
+
+
+if (isTrue) {
+    console.log("Computer is blasting");
+}else{
+    console.log("Computer is not blasting");
+}
+
+document.title = "Hey I am good";
