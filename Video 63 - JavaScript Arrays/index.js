8670ef28be5d513d4a065b9207ba1603371eb5c0
THESV7 <vadhelsmit@gmail.com>
2024-06-24 21:42:26 +0530

    video 63 done

---
+++ Video 63 - JavaScript Arrays/index.js
@@ -0,0 +1,24 @@
+let arr = [1, 2, 4, 5, 7]
+// Index = 0, 1, 2, 3, 4
+
+arr[0] = 5666; // arrays are mutable
+// console.log(arr, typeof arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[4]);
+
+console.log(arr.toString());
+console.log(arr.join(" and "));
+console.log(arr.pop()); // removes the last element
+console.log(arr.push("Harry"));
+/*
+shift()
+unshift()
+delete()
+concat()
+sort()
+splice()
+slice()
+reverse()
+*/ 
