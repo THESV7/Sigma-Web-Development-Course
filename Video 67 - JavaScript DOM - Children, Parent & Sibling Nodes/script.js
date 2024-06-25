223361e2c985f4fcfb43563c514a6faa6bcee204
THESV7 <vadhelsmit@gmail.com>
2024-06-25 20:49:43 +0530

    video 67 done

---
+++ Video 67 - JavaScript DOM - Children, Parent & Sibling Nodes/script.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes // includes comments and text
+document.body.firstElementChild.children // only html elements
