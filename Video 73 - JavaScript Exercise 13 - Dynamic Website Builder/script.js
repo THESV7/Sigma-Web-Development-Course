100ae408b222ca4b5b52ac43b19a0b1a5cdea52d
THESV7 <vadhelsmit@gmail.com>
2024-07-06 10:26:53 +0530

    video 73 done

---
+++ Video 73 - JavaScript Exercise 13 - Dynamic Website Builder/script.js
@@ -0,0 +1,5 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    // Finish this function
+}
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://www.youtube.com/playlist?list=PLu0W_9lII9agq5TrH9XLIKQvv0iaF2X3w")
