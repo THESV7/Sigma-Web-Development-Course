936561d2a5cdc8951b4ee122cbbf825867c96b02
THESV7 <vadhelsmit@gmail.com>
2024-07-18 21:32:09 +0530

    video 80 done

---
+++ Video 80 - Classes & Objects - Object Oriented Programming in Js/gs.js
@@ -0,0 +1,29 @@
+class User {
+
+    constructor(name){
+        //invokes the setter
+        this.name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if(value.length < 4) {
+            console.log("Name is too short.");
+            return;
+        }
+        this._name = value;
+    }
+
+}
+
+// Setters
+let user = new User("John")
+console.log(user.name); // John
+user = new User("Dev"); // Name is too short
+
+// Getters
+user.name = "Harry" 
+console.log(user.name);
